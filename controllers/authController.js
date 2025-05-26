8563fdaa7f61709faeeac238fa09d6a776923d2e
RahulIvan <rahul.chandela@ivanwebsolutions.com>
2025-05-26 14:51:08 +0530

    Revert "test 3"
    This reverts commit b8e1467f1770b4620585deea208bbf62c4e47dcf.

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 const register = async (req, res) => {
     try { 
         console.log('willchanged dsjdj');
-        console.log('willchanged dsjdj');
         
         const { userName, email, password } = req.body;
         const hashed = await bcrypt.hash(password, 10);
